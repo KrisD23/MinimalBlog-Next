5a2b170a875e9d6fb152a1d2c6d7b0f1211c0fcc
Krishan Dwivedi <krishandwive9@gmail.com>
2024-03-24 09:54:23 +0530

    adding styles with global css and css module

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
   const links = [
@@ -8,13 +9,23 @@ const Links = () => {
     { title: "Blog", path: "/blog" },
   ];
 
+  // Temporarty
+  const session = true;
+  const isAdmin = true;
+
   return (
-    <div>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link key={link.title} href={link.path}>
-          {link.title}
-        </Link>
+        <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
