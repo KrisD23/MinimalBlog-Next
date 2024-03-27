d0702310e828eb86a0a17eef0a8cbee442cd56a9
Krishan Dwivedi <krishandwive9@gmail.com>
2024-03-27 15:00:38 +0530

    added a palceholder image for the posts

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -6,7 +6,7 @@ const PostCard = () => {
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src="/post.png" alt="" fill className={styles.img} />
+          <Image src="/post.jpg" alt="" fill className={styles.img} />
         </div>
         <span className={styles.date}>01.01.2024</span>
       </div>
