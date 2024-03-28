45961badc1af4e101f6c046b496b68752df430c4
Krishan Dwivedi <krishandwive9@gmail.com>
2024-03-29 00:03:11 +0530

    added  a way to view the particular post

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,14 +1,27 @@
 import Image from "next/image";
 import styles from "./singlePost.module.css";
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  const post = await getData(slug);
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/post.jpg" alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image
             className={styles.avatar}
@@ -26,12 +39,7 @@ const SinglePostPage = () => {
             <span className={styles.detailValue}>01.02.2024</span>
           </div>
         </div>
-        <div className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
-          quidem earum ipsum saepe reprehenderit repellat debitis deleniti, ad
-          cumque maxime. Amet repellendus iusto eos nobis quo cum eligendi
-          commodi cupiditate!
-        </div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
