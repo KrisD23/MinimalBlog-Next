9dfdcbd0ab322c60cbe68c3796a9df4f4f73f826
Krishan Dwivedi <krishandwive9@gmail.com>
2024-03-30 00:31:06 +0530

    add post and delete post using server actions

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,11 +1,38 @@
-export const addPost = async (formData) => {
-  "use server";
+"use server";
 
+import { revalidatePath } from "next/cache";
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
   //   const title = formData.get("title");
   //   const desc = formData.get("desc");
   //   const slug = formData.get("slug");
 
   const { title, desc, slug, userId } = Object.fromEntries(formData);
 
-  console.log(title, desc, slug, userId);
+  try {
+    connectToDb();
+    const newPost = new Post({ title, desc, slug, userId });
+    await newPost.save();
+    console.log("saved to db");
+    revalidatePath("/blog");
+  } catch (error) {
+    console.log(error);
+    return { error: "something went wrong" };
+  }
+};
+
+export const deletePost = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    await Post.findByIdAndDelete(id);
+    console.log("deleted from ");
+    revalidatePath("/blog");
+  } catch (error) {
+    console.log(error);
+    return { error: "something went wrong" };
+  }
 };
