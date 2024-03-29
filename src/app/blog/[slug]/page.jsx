b8664ad4d18215e760814b33e8cb73667e107ed2
Krishan Dwivedi <krishandwive9@gmail.com>
2024-03-29 17:44:53 +0530

    completed single page and blog page with no cache

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -26,19 +26,14 @@ const SinglePostPage = async ({ params }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image src="/post.jpg" alt="" fill className={styles.img} />
-      </div>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post?.title}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image
-            className={styles.avatar}
-            src="/post.jpg"
-            alt=""
-            width={50}
-            height={50}
-          />
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
               <PostUser userId={post.userId} />
@@ -49,7 +44,7 @@ const SinglePostPage = async ({ params }) => {
             <span className={styles.detailValue}>01.02.2024</span>
           </div>
         </div>
-        <div className={styles.content}>{post.body}</div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   );
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,5 +1,6 @@
 import { getUser } from "@/lib/data";
 import styles from "./postUser.module.css";
+import Image from "next/image";
 
 // Fetch data with an API
 
@@ -22,8 +23,17 @@ async function PostUser({ userId }) {
 
   return (
     <div className={styles.container}>
-      <span className={styles.title}>Author</span>
-      <span className={styles.username}>{user.username}</span>
+      <Image
+        className={styles.avatar}
+        src={user.img ? user.img : "/noavatar.png"}
+        alt=""
+        width={50}
+        height={50}
+      />
+      <div className={styles.text}>
+        <span className={styles.title}>Author</span>
+        <span className={styles.username}>{user.username}</span>
+      </div>
     </div>
   );
 }
